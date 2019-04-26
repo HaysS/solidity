f7f08dfcd3113ca465a8ce1a5ecafd5bf373e3b7
Erik Kundt <erik@ethereum.org>
2019-04-26 17:05:26 +0200

    Fixes Natspec parser error when whitespace is missing.

--- libsolidity/parsing/DocStringParser.cpp
+++ libsolidity/parsing/DocStringParser.cpp
@@ -39,12 +39,12 @@ string::const_iterator skipLineOrEOS(
 	return (_nlPos == _end) ? _end : ++_nlPos;
 }
 
-string::const_iterator firstSpaceOrTab(
+string::const_iterator firstDescDelimiter(
 	string::const_iterator _pos,
 	string::const_iterator _end
 )
 {
-	return boost::range::find_first_of(make_pair(_pos, _end), " \t");
+	return boost::range::find_first_of(make_pair(_pos, _end), " \t:");
 }
 
 string::const_iterator firstWhitespaceOrNewline(
@@ -135,7 +135,7 @@ DocStringParser::iter DocStringParser::parseDocTagParam(iter _pos, iter _end)
 		appendError("No param name given");
 		return _end;
 	}
-	auto nameEndPos = firstSpaceOrTab(nameStartPos, _end);
+	auto nameEndPos = firstDescDelimiter(nameStartPos, _end);
 	auto paramName = string(nameStartPos, nameEndPos);
 
 	auto descStartPos = skipWhitespace(nameEndPos, _end);
---
+++ test/libsolidity/syntaxTests/natspec/doctring_parameter_colon.sol
@@ -0,0 +1,10 @@
+contract C {
+    /**
+     * @dev    : token transfer
+     * @param _from : from address
+     * @param _to   : to address
+     * @param _value: token transfer amount
+     */
+    function _transfer(address _from, address _to, uint256 _value) internal {
+    }
+}
